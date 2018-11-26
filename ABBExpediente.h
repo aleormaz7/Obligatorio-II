3fc8c3bc712d41879958427dff89d16571775289
germanripg <45312492+germanripg@users.noreply.github.com>
2018-11-26 13:54:55 -0300

    Update ABBExpediente.h

--- ABBExpediente.h
+++ ABBExpediente.h
@@ -13,19 +13,15 @@ typedef nodoA * ABBExpediente;
 
 void CrearABB(ABBExpediente &a); ///Crear
 
-boolean PerteneceExp(ABBExpediente a, Expediente e);///Saber si Expediente pertenece al ABB
-
 boolean PerteneceExpPorCod(ABBExpediente a, long int cod); ///Saber si Expediente pertenece al ABB, por su codigo de expediente
 
 void InsertABBExp(ABBExpediente &a, Expediente e); ///PRECONDICION: el Expediente no existe previamente en el ABB
 
-Expediente minimoExpediente (ABBExpediente a) ; /// PrecondiciÛn : el ·rbol a  NO est· vacÌo
+Expediente minimoExpediente (ABBExpediente a) ; /// Precondici√≥n : el √°rbol a  NO est√° vac√≠o
 
-void borrarMinimoExpedienete (ABBExpediente &a); /// PrecondiciÛn : el ·rbol a  NO est· vacÌo
+void borrarMinimoExpedienete (ABBExpediente &a); /// Precondici√≥n : el √°rbol a  NO est√° vac√≠o
 
-void borrarABBExpediente (Expediente e , ABBExpediente &a); /// PrecondiciÛn : el Expediente est· en el ·rbol
-
-void borrarABBExpedientePorCodigo (long int cod , ABBExpediente &a); /// PrecondiciÛn : el Expediente est· en el ·rbol
+void borrarABBExpedientePorCodigo (long int cod , ABBExpediente &a); /// Precondici√≥n : el Expediente est√° en el √°rbol
 
 void ListarExpedientesOrdenados(ABBExpediente a);
 
