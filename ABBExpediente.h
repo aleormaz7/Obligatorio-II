ff8a1d7ce45f456223eee0dda9b65567e503d59a
German <pequerag@gmail.com>
2018-11-25 23:20:31 -0300

    Update ABBExpediente.h

--- ABBExpediente.h
+++ ABBExpediente.h
@@ -1,7 +1,6 @@
 #ifndef ABBEXPEDIENTE_H_INCLUDED
 #define ABBEXPEDIENTE_H_INCLUDED
 
-
 #include "Expediente.h"
 
 typedef struct nodoE{ Expediente info;
@@ -11,16 +10,24 @@ typedef struct nodoE{ Expediente info;
 
 typedef nodoA * ABBExpediente;
 
-///Crear
-void CrearABB(ABBExpediente &a);
 
-///Pertenece
-///Saber si un elemento pertenece al ABB, versión iterativa
-boolean Pertenece(ABBExpediente a, Expediente e);
+void CrearABB(ABBExpediente &a); ///Crear
 
-///Insert
-///PRECONDICION: el valor no existía previamente en el ABB
-void Insert(ABBExpediente &a, Expediente e);
+boolean PerteneceExp(ABBExpediente a, Expediente e);///Saber si Expediente pertenece al ABB
+
+boolean PerteneceExpPorCod(ABBExpediente a, long int cod); ///Saber si Expediente pertenece al ABB, por su codigo de expediente
+
+void InsertABBExp(ABBExpediente &a, Expediente e); ///PRECONDICION: el Expediente no existe previamente en el ABB
+
+Expediente minimoExpediente (ABBExpediente a) ; /// Precondición : el árbol a  NO está vacío
+
+void borrarMinimoExpedienete (ABBExpediente &a); /// Precondición : el árbol a  NO está vacío
+
+void borrarABBExpediente (Expediente e , ABBExpediente &a); /// Precondición : el Expediente está en el árbol
+
+void borrarABBExpedientePorCodigo (long int cod , ABBExpediente &a); /// Precondición : el Expediente está en el árbol
+
+void ListarExpientesOrdenados (ABBExpediente a);
 
 
 
