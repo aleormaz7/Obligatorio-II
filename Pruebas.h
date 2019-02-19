b6bef146585db1f0ef165d783c4f698ed1012925
German <pequerag@gmail.com>
2019-02-19 08:21:02 -0300

    .

--- Pruebas.h
+++ Pruebas.h
@@ -1,9 +1,12 @@
 #ifndef PRUEBAS_H_INCLUDED
 #define PRUEBAS_H_INCLUDED
 #include "ListaString.h"
+#include "ListaTerminos.h"
 
 void pruebaListaStrings ();
 // programa de prueba de la lista de strings
 
+
+void pruebasSumarPolinomios();
 
 #endif // PRUEBAS_H_INCLUDED
