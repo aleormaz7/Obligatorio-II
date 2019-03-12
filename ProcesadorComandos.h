e71c064ce901db9eef568e9e1d126ccf7f05b73f
German <pequerag@gmail.com>
2019-03-11 22:21:08 -0300

    .

--- ProcesadorComandos.h
+++ ProcesadorComandos.h
@@ -5,22 +5,31 @@
 #include "ABBPolinomio.h"
 #include "Archivo.h"
 
+///crear el Polinimio a partir de la lista de String
 void comandoCrear(ABBPolinomio &abb, ListaString ls);
 
+///Suma dos polinomios y genera otro con el resultado
 void comandoSumar(ABBPolinomio &abb, ListaString ls);
 
+///Multiplica dos polinomios y genera otro con el resultado
 void comandoMultiplicar(ABBPolinomio &abb, ListaString ls);
 
+///Evalua un polinomio, para el valor entero
 void comandoEvaluar(ABBPolinomio abb, ListaString ls);
 
+///Evalua si el numero el valor entero
 void comandoEsRaiz(ABBPolinomio abb, ListaString ls);
 
+///lista los polinomios contenidos en memoria(ABB) ordenados alfabeticamente por su nombre
 void comandoMostrar(ABBPolinomio abb, ListaString ls);
 
+///Guarda un polinomio exitente en memoria en el archivo
 void comandoGuardar(ABBPolinomio abb, ListaString ls);
 
+///recupera un polinomio de un archivo y lo carga en el ABB
 void comandoRecuperar(ABBPolinomio &abb, ListaString ls);
 
+///libera la memoria dinamica
 void comandoSalir(ABBPolinomio &abb, boolean &fin, ListaString ls);
 
 #endif // PROCESADORCOMANDOS_H_INCLUDED
