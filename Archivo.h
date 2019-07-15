8e1caa5e5c4bf71f836b66a1d6396b04bf097c57
LAPTOP-HP\Ale <aleormaz07@gmail.com>
2019-07-15 09:06:10 -0300

    delete
    delete

--- Archivo.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ARCHIVO
-#define _ARCHIVO
-
-#include "String.h"
-
-///Determina si existe o no un archivo con el nombre recibido por parámetro
-boolean ExisteArchivo(String nomArch);
-
-#endif // ARCHIVO_H_INCLUDED
--- Boolean.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BOOLEAN
-#define _BOOLEAN
-
-#include <stdio.h>
-
-typedef enum {FALSE,TRUE} boolean;
-
-#endif //
