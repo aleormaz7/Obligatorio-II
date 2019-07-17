acb29e1ce1a130a6bcea4f7c26796d45aaac93b4
German <pequerag@gmail.com>
2019-07-16 21:42:20 -0300

    .

--- Obligatorio/Ciudad.cpp
+++ Obligatorio/Ciudad.cpp
@@ -1,2 +1,26 @@
 #include "Ciudad.h"
 
+void DarNombre(Ciudad c, String &nombre)
+{
+    strcop(nombre,c.nombreCiudad);
+}
+
+int DarCodigo(Ciudad c)
+{
+    return c.codigoCiudad;
+}
+
+void MostrarCiudad(Ciudad c)
+{
+    printf("\nCodigo de la ciudad: %d", c.codigoCiudad);
+    printf("\nNombre: ");
+    print(c.nombreCiudad);
+}
+
+void CargarCiudad(Ciudad &c, int codCiud)
+{
+    c.codigoCiudad = codCiud;
+    printf("\nNombre de la ciudad: ");
+    strcrear(c.nombreCiudad);
+    scan(c.nombreCiudad);
+}
--- Obligatorio/Ciudad.h
+++ Obligatorio/Ciudad.h
@@ -7,4 +7,12 @@ typedef struct{ int codigoCiudad;/*0,1,2,etc*/
               } Ciudad;
 
 
+void DarNombre(Ciudad c, String &nombre);
+
+int DarCodigo(Ciudad c);
+
+void MostrarCiudad(Ciudad c);
+
+void CargarCiudad(Ciudad &c);
+
 #endif // CIUDAD_H_INCLUDED
